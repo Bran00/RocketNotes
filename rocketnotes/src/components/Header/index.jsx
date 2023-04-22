0208158e06f26fc393f563cd23ea9021cf65f32a
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-04-22 16:38:59 -0300

    feat: create place to img

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from './styles';
+import { Container, Profile } from './styles';
 
 export function Header( ) {
 
   return (
    <Container>
+    <Profile>
+
+      <img src="https://github.com/bran00.png" alt="Brando Rocha" />
+
+      <div>
+        <span>Bem Vindo</span>
+        <strong>Brando Rocha</strong>
+      </div>
+
+    </Profile>
    </Container>
   )
 }
