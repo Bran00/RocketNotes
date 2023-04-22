53cdfcac2534e25c54f139908b2537ed671a9012
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-04-22 18:03:32 -0300

    feat: add tags to project

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import { Container } from './styles';
+import { Container, Links } from './styles';
 
-import { Header } from '../../components/Header'
-import { Button } from '../../components/Button'
+import { Header, Button, Section, Tag,  } from '../../components'
 
 export function Details() {
 
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">Item 1</a></li>
+          <li><a href="#">Item 2</a></li>
+          <li><a href="#">Item 3</a></li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   )
