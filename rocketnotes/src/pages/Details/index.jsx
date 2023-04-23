1907f404cf83b8664a78f062ee7b1eecd08c075b
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-04-23 08:15:53 -0300

    feat: finished the details

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Container, Links } from './styles';
+import { Container, Links, Content } from './styles';
 
 import { Header, Button, ButtonText, Section, Tag } from '../../components'
 
@@ -9,22 +9,38 @@ export function Details() {
     <Container>
       <Header />
 
-      <ButtonText title="Excluir Nota"/>
+      <main>
+        <Content>
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">Item 1</a></li>
-          <li><a href="#">Item 2</a></li>
-          <li><a href="#">Item 3</a></li>
-        </Links>
-      </Section>
+          <ButtonText title="Excluir Nota" />
+          <h1>Introdução ao React</h1>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node" />
-      </Section>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, omnis, nam corrupti nulla obcaecati mollitia unde iusto, praesentium natus possimus iste deleniti? Possimus, error! Dolorum quia commodi consequuntur magnam ex?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae explicabo quo rerum, quidem molestiae sint perspiciatis maiores doloribus illum laborum autem quisquam voluptatibus repellat beatae accusamus esse hic sapiente adipisci.
+          </p>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">Item 1</a>
+              </li>
+              <li>
+                <a href="#">Item 2</a>
+              </li>
+              <li>
+                <a href="#">Item 3</a>
+              </li>
+            </Links>
+          </Section>
 
-      <Button title="Voltar" />
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
