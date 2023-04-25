cde19f9cafad02e68a9af86d66325a609147adef
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-04-25 15:25:48 -0300

    feat: includes routes auth

--- notes/src/pages/SignIn/index.jsx
+++ notes/src/pages/SignIn/index.jsx
@@ -14,6 +14,8 @@ export function SignIn( ) {
         <h2>Faça seu login</h2>
 
         <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
         
 
         <Button title="Entrar" />
